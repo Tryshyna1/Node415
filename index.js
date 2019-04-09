ddf34cbe7082bcf5f182d5df2a1d33c579a9e1c0
Tryshyna1 <Volha.tryshyna@selu.edu>
2019-04-09 01:06:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
@@ -10,4 +10,4 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
