b9f0573a931e70ab33eab8682f52bf92323d62ff
Olga Tryshyna <Volha.tryshyna@selu.edu>
2019-04-28 23:35:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 var express = require('express');
+var chalk = require('chalk');
 var app = express();
-var bodyParser = require('body-parser');
-var fs = require ("fs");
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
 
 var port = process.env.PORT || 5000;
 var router = express.Router();
+var ticket = [
+ { "id": 89776,
+ "created_at": "2017-07-20T22:55:29Z",
+ "updated_at": "2017-05-05T10:38:52Z",
+ "type": "incident",
+ "subject": "Printer is not working",
+ "description": "PC Load Letter? What does that even mean???",
+ "priority": "med",
+ "status": "open",
+ "recipient": "support_example@selu.edu",
+ "submitter": "Michael_bolton@selu.edu",
+ "assignee_id": 235378,
+ "follower_ids": [235378, 235],
+ "tags": ["enterprise", "printers"],
 
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-router.get('/listTickets', function(req, res) {
-    fs.readFile("tickets.json", function(err, data) {
-      console.log( data );
-      res.end( data );
-    });
-});
-
-var ticket = {
-  "ticket4": {
+},
+{
+  "id": 876543,
   "created_at": "2017-07-20T22:55:29Z",
   "updated_at": "2017-05-05T10:38:52Z",
   "type": "incident",
-  "subject": "Printer is not working",
+  "subject": "Scanner is not working",
   "description": "PC Load Letter? What does that even mean???",
   "priority": "med",
   "status": "open",
@@ -36,31 +35,75 @@ var ticket = {
   "assignee_id": 235378,
   "follower_ids": [235378, 235],
   "tags": ["enterprise", "printers"],
-   "id": 6
-   }
+},
+{
+  "id": 455653,
+  "created_at": "2017-07-20T22:55:29Z",
+  "updated_at": "2017-05-05T10:38:52Z",
+  "type": "incident",
+  "subject": "MPF is not working",
+  "description": "PC Load Letter? What does that even mean???",
+  "priority": "med",
+  "status": "open",
+  "recipient": "support_example@selu.edu",
+  "submitter": "Michael_bolton@selu.edu",
+  "assignee_id": 235378,
+  "follower_ids": [23537909, 235],
+  "tags": ["enterprise", "printers"],
 }
+];
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+//endpoint to list all tickets
+router.get('/list', function(req, res) {
 
-router.post('/addTicket', function (req, res) {
-   // First read existing users.
-   fs.readFile("tickets.json", function (err, data) {
-      data = JSON.parse( data );
-      data["ticket4"] = ticket["ticket4"];
-      console.log( data );
-      res.end( JSON.stringify(data));
-   });
+res.status(200).send(JSON.stringify(tickets));
 });
 
-router.get('/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile("tickets.json", function (err, data) {
-      var tickets = JSON.parse( data );
-      var ticket = tickets["ticket" + req.params.id]
-      console.log( ticket );
-      res.end( JSON.stringify(ticket));
-   });
+//endpoint to retrieve tickets by id
+
+router.get('/ticket/:id', function (req, res) {
+   //iterate to find the correct object
+   for( i = 0; i<myArray.length, i++; )
+  {
+    if (myArray[i].id == req.params.id)
+  { ticket = myArray[i];
+  }
+
+  }
+  res.status(200).send(JSON.stringify(ticket));
+
+});
+//Endpoint to post a new ticket
+router.post('/ticket', function(req, res) {
+  ticket = {
+    id: req.body.id,
+    created_at: req.body.created_at,
+    updated_at: req.body.updated_at,
+    type: req.body.type,
+    subject: req.body.subject,
+    description: req.body.description,
+
+  };
+  ticket.push(ticket);
+  res.status(200).send(JSON.stringify(ticket));
 });
 
 
-app.use('/api', router);
-app.listen(port);
-console.log('Magic happens on port ' + port);
+
+
+
+
+
+
+
+
+
+
+app.use('/rest', router);
+app.listen(port, function() {
+console.log('Node is running on port ' + port)
+});
